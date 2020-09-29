e1985db1bfac904a7dcc40f11fd11bf4b292bd86
Theo Henson <theodorehenson@protonmail.com>
2020-09-29 15:32:49 -0400

    Add preview sentences

--- script.js
+++ script.js
@@ -32,20 +32,35 @@ function updateMass() {
 
 function onChange(r, g, b) {
 	this.source.value = this.color(r, g, b);
-	this.source.style.backgroundColor = this.color(r, g, b);
 	this.source.style.color = invertColor(CP.HEX(this.get()));
+	var list = document.getElementsByClassName(this.colorClass);
+	for (var name of list) {
+		name.style.backgroundColor = CP.HEX(this.get());
+	}
+	list = document.getElementsByClassName(this.colorClass + "-fg");
+	for (var name of list) {
+		name.style.color = CP.HEX(this.get());
+	}
 	updateMass();
 }
 
 function onInputChange() {
 	this.set.apply(this, CP.HEX(this.source.value)).enter();
-	this.source.style.backgroundColor = CP.HEX(this.get());
 	this.source.style.color = invertColor(CP.HEX(this.get()));
+	var list = document.getElementsByClassName(this.colorClass);
+	for (var name of list) {
+		name.style.backgroundColor = CP.HEX(this.get());
+	}
+	list = document.getElementsByClassName(this.colorClass + "-fg");
+	for (var name of list) {
+		name.style.color = CP.HEX(this.get());
+	}
 	updateMass();
 }
 
 for (i = 0; i < colorNames.length; i++) {
-	colors[i] = new CP(document.getElementById(colorNames[i]));
+	colors[i] = new CP(document.getElementById(colorNames[i] + "-id"));
+	colors[i].colorClass = colorNames[i];
 }
 
 for (i = 0; i < colors.length; i++) {
